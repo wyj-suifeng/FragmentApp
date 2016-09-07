ed5a6d0c7265884a67a15b26a84cbae004a6c961
wyj-suifeng <1041457754@qq.com>
2016-09-07 09:39:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/fragmentapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.lenovo.fragmentapp;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.lenovo.fragmentapp.Words.WordsContent;
+
+public class MainActivity extends Activity implements ItemFragment.OnListFragmentInteractionListener,DetailFragment.OnFragmentInteractionListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(WordsContent.WordItem item) {
+        Bundle arguments =new Bundle();
+        arguments.putString("id",item.id);
+        DetailFragment fragment=new DetailFragment();
+        fragment.setArguments(arguments);
+        getFragmentManager().beginTransaction().replace(R.id.worddetail,fragment).commit();
+
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
